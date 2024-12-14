0ac62bd5e8be0894563f54c3c3cd29807eaa3cb1
Paul-Cavain <paulcavain01@gmail.com>
2024-12-14 03:37:07 +0300

    testimonies added

--- src/Testimonies.js
+++ src/Testimonies.js
@@ -1,22 +1,23 @@
+import Carousels from "./Carousels";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
-const Testimonies  = () => {
-    return(
-        <section>
-            <nav>
-                <Navbar />
-            </nav>
+const Testimonies = () => {
+  return (
+    <section>
+      <nav>
+        <Navbar />
+      </nav>
 
-            <main className="mt-24">
-                <span>Testimonies</span>
-            </main>
+      <main className="mt-24">
+        <Carousels />
+      </main>
 
-            <footer>
-                <Footer />
-            </footer>
-        </section>
-    );
-}
+      <footer>
+        <Footer />
+      </footer>
+    </section>
+  );
+};
 
 export default Testimonies;
